e93caf8d4213d64682171255faa4b32ee55d3240
techshomez <danieltopeg@gmail.com>
2023-08-08 13:55:33 +0100

    Add navbar component

--- src/components/common/navbar/index.jsx
+++ src/components/common/navbar/index.jsx
@@ -1,39 +1,57 @@
 import { Link } from "react-router-dom";
 import "./navbar.scss";
-import { FiSearch, FiShoppingCart, FiUser } from "react-icons/fi";
+import "./res.scss";
+import {
+  FiBox,
+  FiMenu,
+  FiSearch,
+  FiShoppingCart,
+  FiUser,
+} from "react-icons/fi";
 
 const Header = () => {
   return (
     <>
-      <header className="header">
-        <nav className="navbar">
-          <div className="left">
-            <Link to="/">
-              <img src="/images/logo.png" alt="" />
-            </Link>
-          </div>
-          <div className="middle">
-            <Link to="">Home</Link>
-            <Link to="">Home</Link>
-            <Link to="">Home</Link>
-            <Link to="">Home</Link>
-            <Link to="">Home</Link>
-            <Link to="">Home</Link>
-            <Link to="">Home</Link>
-          </div>
-          <div className="right">
-            <i>
-              <FiUser />
-            </i>
-            <i>
-              <FiShoppingCart />
-            </i>
-            <i>
-              <FiSearch />
-            </i>
-          </div>
-        </nav>
-      </header>
+      <main className="main">
+        <header className="header">
+          <nav className="navbar">
+            <div className="left">
+              <Link to="/" className="logo">
+                <i>
+                  <FiBox />
+                </i>
+                <span>FurnishStore</span>
+              </Link>
+            </div>
+            <div className="middle">
+              <Link to="" className="active">
+                Home
+              </Link>
+              <Link to="">About</Link>
+              <Link to="">Furniture</Link>
+              <Link to="">Testimonial</Link>
+              <Link to="">Blog</Link>
+              <Link to="">Contact</Link>
+            </div>
+            <div className="right">
+              <i>
+                <FiUser />
+              </i>
+              <i>
+                <FiShoppingCart />
+              </i>
+              <i className="icon-active">
+                <FiSearch />
+              </i>
+            </div>
+            <div className="hambuger">
+              <i>
+                <FiMenu />
+              </i>
+            </div>
+          </nav>
+        </header>
+      </main>
     </>
   );
 };
