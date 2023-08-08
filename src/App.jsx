ef19173b255f3ec586f868f89eac202fee5a6671
techshomez <danieltopeg@gmail.com>
2023-08-08 12:04:19 +0100

    initializing project & install dependencies

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/common/navbar/header";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/common/navbar/header.jsx
@@ -0,0 +1,7 @@
+import "./navbar.scss";
+
+const Header = () => {
+  return <div>Header</div>;
+};
+
+export default Header;
